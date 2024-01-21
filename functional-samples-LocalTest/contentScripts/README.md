b72d86273d57b57cb1892c9d1631280f97c6ff50
tanken10 <f.kenta89@gmail.com>
2024-01-21 23:32:58 +0900

    静的サイトでのコンテンツスクリプトの動作確認完了

---
+++ functional-samples-LocalTest/contentScripts/README.md
@@ -0,0 +1,8 @@
+# コンテンツスクリプトを試してみる
+参考：https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts?hl=ja
+
+## 静的サイトでの動作確認済み
+やり方
+・拡張機能をChomeに読み込ませる
+・ChomeでwebPage.htmlを開く
+・ボタンを押す
---
+++ functional-samples-LocalTest/contentScripts/content-script.js
@@ -0,0 +1,5 @@
+var greeting = "hola, ";
+var button = document.getElementById("mybutton");
+button.person_name = "Roberto";
+button.addEventListener(
+    "click", () => alert(greeting + button.person_name + "."), false);
