72f3182147d1687a2490c17bde4a53c580874847
tanken10 <f.kenta89@gmail.com>
2024-01-24 18:55:39 +0900

    正常には動いていない。 ただbackgroundスクリプトへざっくり転送できてる ・日付側はHTMLから読み取れてない ・金額側はどれを選んでも戦闘の金額が反映されている

--- functional-samples-LocalTest/amazonClassPress/content-script.js
+++ functional-samples-LocalTest/amazonClassPress/content-script.js
@@ -6,9 +6,13 @@ clickableSpansData.forEach(span => {
     span.addEventListener('click', function () {
         // クリックされたときに実行するコードをここに記述
         alert('日付がクリックされました！');
+        // 日付が読み取れた内容を表示
+        var clickSpansDataElement = document.querySelector('.a-color-base.a-text-bold');
+        var clickSpansDataValue = clickSpansDataElement.textContent;
+        alert(clickSpansDataValue);
         // 他の処理を追加できます
         // backgroundスクリプトへメッセージ送信
-        chrome.runtime.sendMessage('日付クリック', (receive) => {
+        chrome.runtime.sendMessage('日付クリック:' + clickSpansDataValue, (receive) => {
             // 送り返されるものがなければ、このコールバックは必要ない
             console.log(receive); // sendResponse で送り返された {} 空のObject
         });
@@ -24,12 +28,18 @@ clickableSpansPrice.forEach(span => {
     span.addEventListener('click', function () {
         // クリックされたときに実行するコードをここに記述
         alert('金額がクリックされました！');
+        // 日付が読み取れた内容を表示
+        var clickSpansPriceElement = document.querySelector('.a-price-whole');
+        var clickSpansPriceValue = clickSpansPriceElement.textContent;
+        alert(clickSpansPriceValue);
         // 他の処理を追加できます
         // backgroundスクリプトへメッセージ送信
-        chrome.runtime.sendMessage('金額クリック', (receive) => {
+        chrome.runtime.sendMessage('金額クリック:' + clickSpansPriceValue, (receive) => {
             // 送り返されるものがなければ、このコールバックは必要ない
             console.log(receive); // sendResponse で送り返された {} 空のObject
         });
     });
 });
+
+
 
